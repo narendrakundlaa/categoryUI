0cc675bf0fb66fe05c451e56c91b1e984018a171
kundlaanarendra <40867834+narendrakundlaa@users.noreply.github.com>
2019-10-21 18:44:43 +0530

    Update upload-data.service.ts

--- src/app/shared-components/upload-data.service.ts
+++ src/app/shared-components/upload-data.service.ts
@@ -11,11 +11,12 @@ export class UploadDataService {
   fileUploadUrl: string = environment.baseUrl + 'categories/products';
   constructor(private httpClient: HttpClient) { }
 
-  sendFiles(event) {
+  sendFiles(event,name) {
     const formData = new FormData();
     console.log('1', event.target.file);
     console.log('2', event.target.files[0])
     formData.append('file', event.target.files[0]);
+    formData.append('name', name);
 
     return this.httpClient.post(this.fileUploadUrl, formData);
   }
